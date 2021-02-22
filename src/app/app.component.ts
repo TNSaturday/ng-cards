68cdd24183c013535c168f5716f354e6f1656d5c
TSubbota <tsubbota@nativeroll.tv>
2021-02-22 22:08:35 +0300

    Create separate card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountain',
+      content: 'I saw this mountain today'
+    },
+    {
+      title: 'Bicycle',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biker',
+      content: 'I hate typing so much'
+    }
+  ];
   title = 'ng-cards';
 }
